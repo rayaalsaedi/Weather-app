a3aac6a80f0eadcbab508956fdce183006528735
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-03-12 16:16:31 -0800

    updates to js and css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const weather = {
     apiKey: "5ef023d1a3a07faee421b4e629dd17e0",
+    localStorage: JSON.parse(localStorage.getItem("city")) || [],
     // find weather data by city name
     findWeather: function (city) {
         //fetch URL
@@ -14,23 +15,65 @@ const weather = {
     },
     // function to display the weather on the page
     showWeather: function (data) {
+
         // get city name from the  data 
         const { name } = data;
         // get temp and humidity name from the data under main 
         const { temp, humidity } = data.main;
         // get speed from data under wind
         const { speed } = data.wind;
-
+        const { icon } = data.weather[0];
+        const { lon, lat } = data.coord;
+        console.log(icon)
+        this.createLi(name)
+        this.localStorage.push(name)
+        localStorage.setItem("city", JSON.stringify(this.localStorage))
+        const img = document.createElement("img");
+        img.setAttribute("src", "http://openweathermap.org/img/w/" + icon + ".png");
         console.log(name, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").append(img)
         document.querySelector(".temp").innerText = temp + " ℃";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        this.fiveDays(name)
+        fetch(
+            `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${this.apiKey}`
+        )
+            // Once URL fetched then use response.json to break down the information
+            .then((response) => response.json())
+            //Display data 
+            .then((data) => {
+                console.log(data);
+                document.querySelector(".uv").innerText = "UV Index: " + data.value;
+            });
     },
+
     // function to look up the input value and gets it from the findWeather
     search: function () {
         this.findWeather(document.querySelector("#inputKey").value);
     },
+    createLi: function (city) {
+        const li = document.createElement("li").textContent = city
+        document.querySelector(".search-history").append(li)
+    },
+    fiveDays: function (city) {
+        fetch(
+            "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=" + this.apiKey
+        )
+            // Once URL fetched then use response.json to break down the information
+            .then((response) => response.json())
+            //Display data 
+            .then((data) => {
+                console.log(data)
+                for (let i = 0; i < data.list.length; i++) {
+                    const element = data.list[i];
+                    if (element.dt_txt.indexOf("12:00:00") !== -1) {
+                        console.log(element)
+                    }
+                }
+            });
+    }
 };
 
 // activate the search button using the search function above
@@ -38,9 +81,8 @@ document.querySelector("#go-button").addEventListener("click", function () {
     weather.search();
 })
 
-//add local storage set item and get item+add search history to the html
-//figure out the UV index 
-
+// box invisible using coding quiz method
+// arrange data in the five days 
 
 
 
